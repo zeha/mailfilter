c38b3f30844d8633c0831f62b7e521f5556b48df
Christian Hofstaedtler <ch@zeha.at>
2004-07-26 22:13:59 +0000

    missing files...huh...

---
+++ iXplat/iXDir.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "iX.h"
+
+#ifndef _IXDIR_H_
+#define _IXDIR_H_
+
+
+class iXDir
+{
+public:
+	iXDir(const char* DirectoryName);
+	virtual ~iXDir();
+	
+	bool ReadNextEntry();
+	const char* GetCurrentEntryName();
+	long long GetCurrentEntrySize();
+	time_t GetCurrentEntryModificationTime();
+	
+	bool SkipDotFiles;
+	
+	bool UnlinkCurrentEntry();
+	
+private:
+	char* m_DirectoryName;
+
+#ifdef IXPLAT_NETWARE_LIBC
+	DIR* m_Directory;
+	struct dirent* m_Entry;
+#endif
+#ifdef IXPLAT_NETWARE_CLIB
+	DIR* m_Directory;
+	DIR* m_Entry;
+#endif
+
+};
+
+#endif // _IXDIR_H_
---
+++ iXplat/source/iXDir.cpp
@@ -0,0 +1,138 @@
+
+#include <dirent.h>
+#include <string.h>
+#include <malloc.h>
+
+#include "iX.h"
+#include "iXDir.h"
+#include <sys/stat.h>
+
+
+iXDir::iXDir(const char* DirectoryName)
+{
+#ifdef IXPLAT_NETWARE_CLIB
+	char* szFull = (char*)malloc(IX_PATH_MAX);
+	strcpy(szFull,DirectoryName);
+	strcat(szFull,"\\*.*");
+	m_Directory = opendir(szFull);
+#else
+	m_Directory = opendir(DirectoryName);
+#endif
+
+	m_Entry = NULL;
+	SkipDotFiles = false;
+	
+	m_DirectoryName = strdup(DirectoryName);
+}
+
+
+iXDir::~iXDir()
+{
+	if (m_Directory != NULL)
+		closedir(m_Directory);
+	
+	if (m_DirectoryName != NULL)
+		free(m_DirectoryName);
+}
+
+bool iXDir::ReadNextEntry()
+{
+	if (m_Directory != NULL)
+	{
+		m_Entry = readdir(m_Directory);
+		
+		// valid result?
+		if (m_Entry == NULL) 
+			return false;
+		else
+		{
+			const char* e = GetCurrentEntryName();
+			if (e[0] == '.')
+			{
+				if (e[1] == '\0')
+				{
+					return ReadNextEntry();
+				} else
+				if ( (e[1] == '.') && (e[2] == '\0') )
+				{
+					return ReadNextEntry();
+				} else
+				if (SkipDotFiles)
+				{
+					// user doesn't want .-files, so we skip over it
+					return ReadNextEntry();
+				} else
+				{
+					return true;
+				}
+			} else 
+			{
+				// no dotfile
+				return true;
+			}
+
+			
+		}
+	} else
+		return false;
+}
+
+const char* iXDir::GetCurrentEntryName()
+{
+	if (m_Entry != NULL)
+	{
+#ifdef IXPLAT_NETWARE_CLIB
+		return m_Entry->d_nameDOS;
+#else
+		return m_Entry->d_name;
+#endif
+ 	} else{
+ 		return "";
+ 	}
+}
+
+long long iXDir::GetCurrentEntrySize()
+{
+	if (m_Entry != NULL)
+		return m_Entry->d_size;
+	else
+		return 0;
+}
+
+bool iXDir::UnlinkCurrentEntry()
+{
+	if (m_Entry != NULL)
+	{
+		const char* e = GetCurrentEntryName();
+		
+		char* fullE = (char*)malloc(strlen(e) + strlen(m_DirectoryName) + strlen(IX_DIRECTORY_SEPARATOR_STR)*2 + 1);
+		
+		strcpy(fullE,m_DirectoryName);
+		strcat(fullE,IX_DIRECTORY_SEPARATOR_STR);
+		strcat(fullE,e);
+		
+		if (unlink(fullE))
+			return false;
+		else
+			return true;
+	} else
+		return false;
+}
+
+time_t iXDir::GetCurrentEntryModificationTime()
+{
+	if (m_Entry != NULL)
+	{
+		struct stat st;
+		const char* e = GetCurrentEntryName();
+		
+		stat(e,&st);
+#ifdef IXPLAT_NETWARE_LIBC
+		return st.st_mtime.tv_sec;
+#else
+		return st.st_mtime;
+#endif
+	
+	} else
+		return 0;
+}
